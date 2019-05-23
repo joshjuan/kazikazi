f6bbed7a37b8ca1eaebc90d61fefc8819a5f3ce3
Joshua Nelson <nelsonjoshua301@gmail.com>
2019-05-24 00:21:50 +0300

    from josh

--- backend/views/user/_form.php
+++ backend/views/user/_form.php
@@ -2,7 +2,6 @@
 
 use backend\models\User;
 use kartik\select2\Select2;
-use yii\helpers\ArrayHelper;
 use yii\helpers\Html;
 use kartik\form\ActiveForm;
 use yii\web\View;
@@ -10,11 +9,16 @@ use yii\web\View;
 /* @var $this yii\web\View */
 /* @var $model backend\models\User */
 /* @var $form \yii\widgets\ActiveForm */
-
-
-$countries = ArrayHelper::map(\backend\models\Region::find()->asArray()->all(), 'id', 'name');
-$cities = ArrayHelper::map(\backend\models\District::find()->asArray()->all(), 'id', 'name');
 ?>
+
+<p style="padding-top: 15px"/>
+<center>
+    <h3>
+        <i class="fa fa-th text-default">
+            <strong> UPDATE FORM</strong>
+        </i>
+    </h3>
+</center>
 <div class="employee-loans-index" style="padding-top: 25px">
     <div class="panel panel-default">
         <div class="user-index" style="padding-top: 40px;padding-left: 10px">
@@ -31,53 +35,52 @@ $cities = ArrayHelper::map(\backend\models\District::find()->asArray()->all(), '
 
                 <div class="col-xs-12 col-lg-12 col-sm-12">
                     <div class="col-sm-12 no-padding">
-                        <div class="col-sm-4">
+                        <div class="col-sm-3">
                             <?= $form->field($model, 'name')->textInput(['maxlength' => true]) ?>
                         </div>
-                        <div class="col-sm-4">
+                        <div class="col-sm-3">
                             <?= $form->field($model, 'username')->textInput(['maxlength' => true]) ?>
                         </div>
-                        <div class="col-sm-4">
+                        <div class="col-sm-3">
+                            <?= $form->field($model, 'region')->widget(Select2::classname(), [
+                                'data' => \backend\models\Region::getRegion(),
+                                'options' => ['placeholder' => 'Choose Region'],
+                                'pluginOptions' => [
+                                    'allowClear' => true,
 
-                            <?= $form->field($model, 'region')->dropDownList(
-                                $countries,
-                                [
-                                    'prompt' => 'Select Region',
-                                    'onchange' => '
-	                                    $.post("index.php?r=user/region-list&id="+$(this).val(), function( data ) {
-	                                   $( "select#city" ).html( data );
-	                                  });'
-                                ]
-                            ); ?>
+                                ],
+                            ]);
+                            ?>
+                            <?php //$form->field($model, 'region')->textInput(['maxlength' => true]) ?>
+                        </div>
+
+
+                        <div class="col-sm-3">
+
+                            <?= $form->field($model, 'district')->widget(Select2::classname(), [
+                                'data' => \backend\models\District::getDistrict(),
+                                'options' => ['placeholder' => 'Choose District'],
+                                'pluginOptions' => [
+                                    'allowClear' => true,
+
+                                ],
+                            ]);
+                            ?>
                         </div>
                     </div>
                     <div class="col-sm-12 no-padding">
-                        <div class="col-sm-4">
-                            <?php echo $form->field($model, 'district')
-                                ->dropDownList(
-                                    $cities,
-                                    [
-                                        'prompt' => 'Select District', 'id' => 'city',
-                                        'onchange' => '
-	                                    $.post("index.php?r=user/district-list&id="+$(this).val(), function( data ) {
-	                                   $( "select#city1" ).html( data );
-	                                  });'
-                                    ]
-                                ); ?>
+                        <div class="col-sm-3">
+                            <?= $form->field($model, 'municipal')->widget(Select2::classname(), [
+                                'data' => \backend\models\Municipal::getMunicipal(),
+                                'options' => ['placeholder' => 'Choose Municipal'],
+                                'pluginOptions' => [
+                                    'allowClear' => true,
 
+                                ],
+                            ]);
+                            ?>
                         </div>
-                        <div class="col-sm-4">
-
-                            <?php echo $form->field($model, 'municipal')
-                                ->dropDownList(
-                                    $cities,
-                                    [
-                                        'prompt' => 'Select District', 'id' => 'city1'
-                                    ]
-                                ); ?>
-
-                        </div>
-                        <div class="col-sm-4">
+                        <div class="col-sm-3">
                             <?= $form->field($model, 'street')->widget(Select2::classname(), [
                                 'data' => \backend\models\Street::getStreet(),
                                 'options' => ['placeholder' => 'Choose Street'],
@@ -88,9 +91,10 @@ $cities = ArrayHelper::map(\backend\models\District::find()->asArray()->all(), '
                             ]);
                             ?>
                         </div>
-                    </div>
-                    <div class="col-sm-12 no-padding">
-                        <div class="col-sm-4">
+
+
+                        <div class="col-sm-3">
+
                             <?= $form->field($model, 'work_area')->widget(Select2::classname(), [
                                 'data' => \backend\models\WorkArea::getWorkArea(),
                                 'options' => ['placeholder' => 'Choose work area'],
@@ -101,20 +105,23 @@ $cities = ArrayHelper::map(\backend\models\District::find()->asArray()->all(), '
                             ]);
                             ?>
                         </div>
-                        <div class="col-sm-4">
+                        <div class="col-sm-3">
                             <?= $form->field($model, 'mobile')->textInput(['maxlength' => true]) ?>
                         </div>
-                        <div class="col-sm-4">
-                            <?= $form->field($model, 'email')->textInput(['maxlength' => true]) ?>
-                        </div>
                     </div>
+
 
                     <div class="col-sm-12 no-padding">
 
                         <div class="col-sm-6">
+                            <?= $form->field($model, 'email')->textInput(['maxlength' => true]) ?>
+                        </div>
+
+
+                        <div class="col-sm-3">
                             <?= $form->field($model, 'role')->dropDownList(\backend\models\User::getArrayRole(), ['prompt' => '-- select Role name --']) ?>
                         </div>
-                        <div class="col-sm-6">
+                        <div class="col-sm-3">
                             <?= $form->field($model, 'status')->dropDownList(\backend\models\User::getArrayStatus()) ?>
                         </div>
                     </div>
@@ -133,7 +140,7 @@ $cities = ArrayHelper::map(\backend\models\District::find()->asArray()->all(), '
                             <?= Html::submitButton($model->isNewRecord ? Yii::t('app', 'Save') : Yii::t('app', 'Update'), ['class' => $model->isNewRecord ? 'btn btn-block btn-success' : 'btn btn-block btn-info']) ?>
                         </div>
                         <div class="col-xs-6 col-xs-12">
-                            <?= Html::a(Yii::t('app', 'Cancel'), ['index'], ['class' => 'btn btn-warning btn-block']) ?>
+                            <?= Html::a(Yii::t('app', 'Cancel'), ['managers-list'], ['class' => 'btn btn-warning btn-block']) ?>
                         </div>
                     </div>
                     <?php ActiveForm::end(); ?>
